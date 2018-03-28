95aba1110ccec266283eb04009a16fe65bbd93f5
Daniel Rocha <daniel@rocha.io>
2018-03-28 10:08:34 +0200

    Add htpdate.exe to .gitignore

--- htpdate.go
+++ htpdate.go
@@ -31,6 +31,7 @@ func timeDiff(url string) (float64, float64, error) {
 	if err != nil {
 		return 0, 0, err
 	}
+
 	date := resp.Header.Get("Date")
 	if date == "" {
 		return 0, 0, errors.New("Date header is missing")
