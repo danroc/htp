5716d3dbb8248daee03ae5bdc4c8ce28c30d1c50
Daniel Rocha <daniel@rocha.io>
2018-10-31 19:23:07 +0100

    Complete remake

---
+++ htp.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math"
+	"net/http"
+	"time"
+)
+
+func main() {
+	n := flag.Uint("n", 12, "Number of requests")
+	h := flag.String("u", "https://google.com", "Host URL")
+	flag.Parse()
+
+	lo, hi := math.Inf(-1), math.Inf(+1)
+	for i := uint(0); i < *n; i++ {
+		t0 := time.Now()
+		resp, err := http.Head(*h)
+		t1 := time.Now()
+		if err != nil {
+			log.Fatal(err)
+		}
+		date := resp.Header.Get("Date")
+		if date == "" {
+			log.Fatal("Date header is missing")
+		}
+		t2, err := time.Parse(time.RFC1123, date)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		u2 := t2.UnixNano()
+		d0 := float64(t0.UnixNano()-u2) / 1e9
+		d1 := float64(t1.UnixNano()-u2) / 1e9
+
+		lo = math.Max(lo, d0-1)
+		hi = math.Min(hi, d1)
+	}
+	fmt.Println((hi + lo) / 2)
+}
