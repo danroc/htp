a33820759af3e150b950f2f4de60b91e8de6a7d2
Daniel Rocha <daniel@rocha.io>
2022-04-23 22:23:50 +0200

    refactor: remove sync.bat script since no longer needed

--- sync.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-if not "%1"=="am_admin" (
-    powershell Start -Verb RunAs '%0' am_admin & exit /b
-)
-
-cd /d "%~dp0"
-powershell -Command "Set-Date -Adjust $([TimeSpan]::FromSeconds($(.\htp.exe -v)))"
-pause
